6d294ef4d1cf7e24bca873ce64d3e63f13a2c2ea
soleribbon <ivar202@gmail.com>
2023-04-18 15:32:48 +0200

    Initial Commit

---
+++ Views/HeaderView.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+
+struct HeaderView: View {
+    
+    //Extra - to use if I want to display the app version
+    let officialAppVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+    
+    
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text("BlindingFX")
+                    .bold()
+                    .font(.title)
+                    .foregroundColor(.primary)
+                
+                Text("Unleash your inner DJ")
+                    .font(.title3)
+                    .foregroundColor(.primary)
+                
+            }
+            Spacer()
+        }
+    }
+}
