b9cd6f758af3199597a2356a91f5228eff1ecf4a
soleribbon <ivar202@gmail.com>
2024-04-05 20:59:06 +0200

    Initial Commit

---
+++ BlindingFX/BlindingFXApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlindingFXApp.swift
+//  BlindingFX
+//
+//  Created by Ravi Heyne on 05/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BlindingFXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlindingFX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlindingFX
+//
+//  Created by Ravi Heyne on 05/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
